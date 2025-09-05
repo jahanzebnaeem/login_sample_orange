497413933124a5745d6af0874ac59f9760cf04dc
Jahanzeb Naeem (Jazy) <jahanzeb.naeem@hotmail.com>
2025-09-05 23:35:19 +0500

    Add simple animation to the project by adding the simple_animations package along with the setup of the fadeanimation.dart file.

---
+++ lib/animation/fadeanimation.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  const FadeAnimation({super.key, required this.delay, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return PlayAnimationBuilder<double>(
+      delay: Duration(milliseconds: (500 * delay).round()),
+      duration: const Duration(milliseconds: 500),
+      tween: Tween(begin: 0.0, end: 1.0),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: Transform.translate(
+            offset: Offset(0, -30 * (1 - value)),
+            child: child,
+          ),
+        );
+      },
+      child: child,
+    );
+  }
+}
